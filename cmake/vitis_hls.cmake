b48f720ba410735f429497dc2f7e8156c05bcf31
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-07-10 21:16:32 +0200

    Fixes for multiple solutions
    Add additional logic to support multiple solutions
    per project
    
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- cmake/vitis_hls.cmake
+++ cmake/vitis_hls.cmake
@@ -17,10 +17,13 @@
 #
 # Generate a Vitis Tcl script from CMake
 #
-# .. note:: this macro will only command CMake to generate
+# .. note:: this macro will command CMake to generate
 # the corresponding Tcl script according to the parameters
-# designed below. To execute such script (e.g. via 
-# "vitis_hls -f <tcl-script>"), refer to ROS 2 colcon extensions.
+# designed below. To execute such script use either Vitis CLI 
+# (e.g. via "vitis_hls -f <tcl-script>") or colcon krs hls.
+#
+# .. note:: the macro will generate one solution per each clock
+# option provided
 #
 # :param PROJECT: Name of the Vitis HLS project to be created
 # :type target: string
@@ -35,7 +38,8 @@
 # :param TESTBENCH: source file for the testbench. Relative path.
 # :type target: string
 #
-# :param CLOCK: Clock period in nanoseconds (e.g. 4)
+# :param CLOCK: Clock period in nanoseconds (e.g. 4). One solution will be
+# generated per each clock element provided.
 # :type target: string
 #
 # :param HEADERS: one or muliple relative path directories whereto search for
@@ -53,88 +57,104 @@
 #
 macro(vitis_hls_generate_tcl)
     set(options SYNTHESIS RTLSIMULATION RTLIMPLEMENTATION)
-    set(oneValueArgs PROJECT TOPFUNCTION TESTBENCH CLOCK)
-    set(multiValueArgs SRC HEADERS)
+    set(oneValueArgs PROJECT TOPFUNCTION TESTBENCH)
+    set(multiValueArgs SRC HEADERS CLOCK)
 
     cmake_parse_arguments(VITIS_HLS "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
 
     # if (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64" AND DEFINED CMAKE_SYSROOT})
     if (DEFINED CMAKE_SYSROOT)  # cross compilation, DEFINED CMAKE_SYSROOT
-      
-      # define template, with or without include directories      
+
+      # set some initial placeholders
+      set(VITIS_HLS_PROJECT_VALUE ${VITIS_HLS_PROJECT})
+      set(VITIS_HLS_TOPFUNCTION_VALUE ${VITIS_HLS_TOPFUNCTION})
+
+      # define template, with or without include directories
+
+      FILE(WRITE ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+      "open_project -reset @VITIS_HLS_PROJECT_VALUE@\n"
+      "add_files @VITIS_HLS_SRC_VALUE@\n"
+      )
+
       if(DEFINED VITIS_HLS_HEADERS)
         # convert to "value1 value2"
         foreach(VITIS_HLS_HEADERS_file ${VITIS_HLS_HEADERS})
           set(VITIS_HLS_HEADERS_VALUE "${VITIS_HLS_HEADERS_VALUE} ${CMAKE_SOURCE_DIR}/${VITIS_HLS_HEADERS_file}")
         endforeach()
 
-        FILE(WRITE ${CMAKE_BINARY_DIR}/hls.tcl.in
-        "open_project -reset @VITIS_HLS_PROJECT_VALUE@\n"
-        "add_files @VITIS_HLS_SRC_VALUE@\n"
+        FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
         "add_files -tb @VITIS_HLS_TESTBENCH_VALUE@ -cflags \"-isystem @VITIS_HLS_HEADERS_VALUE@\"\n"
-        "set_top @VITIS_HLS_TOPFUNCTION_VALUE@\n"
-        "open_solution default\n"  # default solution
-        "set_part {${FIRMWARE_SOC}}\n"
-        "create_clock -period @VITIS_HLS_CLOCK_VALUE@\n"
-        "csim_design -ldflags \"-lOpenCL\" -profile\n"
-      )
+        )
       else()
-        FILE(WRITE ${CMAKE_BINARY_DIR}/hls.tcl.in
-          "open_project -reset @VITIS_HLS_PROJECT_VALUE@\n"
-          "add_files @VITIS_HLS_SRC_VALUE@\n"
+        FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
           "add_files -tb @VITIS_HLS_TESTBENCH_VALUE@\n"
-          "set_top @VITIS_HLS_TOPFUNCTION_VALUE@\n"
-          "open_solution default\n"  # default solution
-          "set_part {${FIRMWARE_SOC}}\n"
-          "create_clock -period @VITIS_HLS_CLOCK_VALUE@\n"
-          "csim_design -ldflags \"-lOpenCL\" -profile\n"
         )
       endif()  # VITIS_HLS_HEADERS, add include directories      
 
-      if (${VITIS_HLS_SYNTHESIS})
-        FILE(APPEND ${CMAKE_BINARY_DIR}/hls.tcl.in
-          "csynth_design\n"
+      FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+      "set_top @VITIS_HLS_TOPFUNCTION_VALUE@\n"
+      )
+      
+      foreach(clock_item ${VITIS_HLS_CLOCK})
+        FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+        "# solution_${clock_item}ns\n"  # mark each solution with clock ns
+        "open_solution solution_${clock_item}ns\n"  # mark each solution with clock ns
+        "set_part {${FIRMWARE_SOC}}\n"
+        "create_clock -period ${clock_item}\n"
+        "csim_design -ldflags \"-lOpenCL\" -profile\n"
         )
-      endif()  # VITIS_HLS_SYNTHESIS
 
-      if (${VITIS_HLS_RTLSIMULATION})
-        FILE(APPEND ${CMAKE_BINARY_DIR}/hls.tcl.in
-          "cosim_design\n"
-        )
-      endif()  # VITIS_HLS_RTLSIMULATION
+        if (${VITIS_HLS_SYNTHESIS})
+          FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+            "csynth_design\n"
+          )
+        endif()  # VITIS_HLS_SYNTHESIS
 
-      if (${VITIS_HLS_RTLIMPLEMENTATION})
-        FILE(APPEND ${CMAKE_BINARY_DIR}/hls.tcl.in
-          "export_design\n"
-        )
-      endif()  # VITIS_HLS_RTLIMPLEMENTATION
+        if (${VITIS_HLS_RTLSIMULATION})
+          FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+            "cosim_design\n"
+          )
+        endif()  # VITIS_HLS_RTLSIMULATION
+
+        if (${VITIS_HLS_RTLIMPLEMENTATION})
+          FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+            "export_design\n"
+          )
+        endif()  # VITIS_HLS_RTLIMPLEMENTATION
 
       # exit at the end
-      FILE(APPEND ${CMAKE_BINARY_DIR}/hls.tcl.in
+      FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
+      "\n"
+      )
+      endforeach()  # for each clock_item
+
+      # exit at the end
+      FILE(APPEND ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in
       "exit\n"
       )
-
-      # set placeholders
-      set(VITIS_HLS_PROJECT_VALUE ${VITIS_HLS_PROJECT})
+      
+      # set placeholders      
       # set(VITIS_HLS_SRC_VALUE ${VITIS_HLS_SRC})  # this gets a list as "value1;value2"
       # we need instead: "value1 value2"
       foreach(VITIS_HLS_SRC_file ${VITIS_HLS_SRC})
         set(VITIS_HLS_SRC_VALUE "${VITIS_HLS_SRC_VALUE} ${CMAKE_SOURCE_DIR}/${VITIS_HLS_SRC_file}")
       endforeach()
-      set(VITIS_HLS_TESTBENCH_VALUE ${CMAKE_SOURCE_DIR}/${VITIS_HLS_TESTBENCH})
-      set(VITIS_HLS_TOPFUNCTION_VALUE ${VITIS_HLS_TOPFUNCTION})
-      set(VITIS_HLS_CLOCK_VALUE ${VITIS_HLS_CLOCK})
+      set(VITIS_HLS_TESTBENCH_VALUE ${CMAKE_SOURCE_DIR}/${VITIS_HLS_TESTBENCH})      
+      # set(VITIS_HLS_CLOCK_VALUE ${VITIS_HLS_CLOCK})  # not necessary anymore, iterating over clocks above
       
       # replace placeholders and produce final Tcl script
-      configure_file(${CMAKE_BINARY_DIR}/hls.tcl.in ${CMAKE_BINARY_DIR}/hls.tcl)
+      configure_file(${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl.in ${CMAKE_BINARY_DIR}/${VITIS_HLS_PROJECT}.tcl)
 
       # install Tcl script
-      install(
-        FILES
-          ${CMAKE_BINARY_DIR}/hls.tcl
-        DESTINATION
-          lib/${PROJECT_NAME}
-      )
+      ## NOTE: this would move .tcl scripts to the install-* directory. Instead
+      ## it's recommended to keep things in build-*, rationale is that install-* 
+      ## is what we're deploying to the embedded platforms and we want this to be compact
+      # install(
+      #   FILES
+      #     ${CMAKE_BINARY_DIR}/hls.tcl
+      #   DESTINATION
+      #     lib/${PROJECT_NAME}
+      # )
     endif()  # cross compilation, DEFINED CMAKE_SYSROOT
 endmacro()  # vitis_acceleration_kernel
