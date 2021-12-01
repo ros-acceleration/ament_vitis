91853d619717caefb8c82324704da9e77db78592
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-12-01 18:05:20 +0100

    Refactor vitis_acceleration_kernel, add clock and link capabilities
    vitis_acceleration_kernel CMake macro has been refactored, code
    has been extended to include separated build, link and package
    capabilities (now specified from the macro invocations directly
    ).
    
    A new clock argument has been added which allows to also set the
    target clock (in Hz) while building the Xilinx Object (.xo) file.
    This capability is redundant to specifying the clock in the .cfg
    files for the build process, which can be done as follows:
    
    [hls]
    clock=300000000:vadd_accelerated
    
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- cmake/vitis_acceleration_kernel.cmake
+++ cmake/vitis_acceleration_kernel.cmake
@@ -31,6 +31,12 @@
 # for the kernel e.g. src/zcu102.cfg.
 # :type target: string
 #
+# :param CLOCK: Frequency in Hz at which the kernel should be compiled by
+# Vitis HLS.
+# See https://www.xilinx.com/html_docs/xilinx2020_2/vitis_doc/vitiscommandcompiler.html#mcj1568640526180__section_bh5_dg4_bjb
+# for more details.
+# :type target: string
+#
 # :param TYPE: type of kernel to generate, could be "sw_emu",
 # "hw_emu" or "hardware" (or several of them)
 # :type target: string
@@ -40,53 +46,80 @@
 # are relative to CMAKE_SOURCE_DIR.
 # :type target: string
 #
+# :param LINK: Invoke the v++ linker to generate the corresponding artifacts.
+# :type target: bool
+#
 # :param PACKAGE: Invoke the -p (package) flag of v++ to generate
 # the corresponding artifacts for emulation.
 # :type target: bool
 #
 macro(vitis_acceleration_kernel)
-    set(options PACKAGE)
-    set(oneValueArgs NAME FILE CONFIG)
+    set(options PACKAGE LINK)
+    set(oneValueArgs NAME FILE CONFIG CLOCK)
     set(multiValueArgs TYPE INCLUDE)
 
     cmake_parse_arguments(VITIS_KERNEL "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
 
+
     # if (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64" AND DEFINED CMAKE_SYSROOT})
     if (DEFINED CMAKE_SYSROOT)
       # debug()
       foreach(type ${VITIS_KERNEL_TYPE})
+        # Check that the build type is valid
         if (${type} STREQUAL "sw_emu")
-          vitis_acceleration_kernel_aux(
-            NAME ${VITIS_KERNEL_NAME}
-            FILE ${VITIS_KERNEL_FILE}
-            TYPE "sw_emu"
-            CONFIG ${VITIS_KERNEL_CONFIG}
-            INCLUDE ${VITIS_KERNEL_INCLUDE}
-            PACKAGE ${VITIS_KERNEL_PACKAGE}
-          )
+          set(BUILT_TYPE "sw_emu")
         elseif (${type} STREQUAL "hw_emu")
-          vitis_acceleration_kernel_aux(
-            NAME ${VITIS_KERNEL_NAME}
-            FILE ${VITIS_KERNEL_FILE}
-            TYPE "hw_emu"
-            CONFIG ${VITIS_KERNEL_CONFIG}
-            INCLUDE ${VITIS_KERNEL_INCLUDE}
-            PACKAGE ${VITIS_KERNEL_PACKAGE}
-          )
+          set(BUILT_TYPE "hw_emu")
         elseif (${type} STREQUAL "hw")
-          vitis_acceleration_kernel_aux(
-            NAME ${VITIS_KERNEL_NAME}
-            FILE ${VITIS_KERNEL_FILE}
-            TYPE "hw"
-            CONFIG ${VITIS_KERNEL_CONFIG}
-            INCLUDE ${VITIS_KERNEL_INCLUDE}
-            PACKAGE ${VITIS_KERNEL_PACKAGE}
-          )
+          set(BUILT_TYPE "hw")
         else()
           message(
             FATAL_ERROR "'" ${type} "' is not a recognized build target \
             for acceleration kernels. Consider 'sw_emu', 'hw_emu' or 'hardware'."
+          )
+        endif()
+
+        # Consider the different link/package options
+        if (${VITIS_KERNEL_LINK} AND ${VITIS_KERNEL_PACKAGE})
+          vitis_acceleration_kernel_aux(
+            NAME ${VITIS_KERNEL_NAME}
+            FILE ${VITIS_KERNEL_FILE}
+            TYPE ${BUILT_TYPE}
+            CLOCK ${VITIS_KERNEL_CLOCK}
+            CONFIG ${VITIS_KERNEL_CONFIG}
+            INCLUDE ${VITIS_KERNEL_INCLUDE}
+            LINK
+            PACKAGE
+          )
+        elseif(${VITIS_KERNEL_LINK})
+          vitis_acceleration_kernel_aux(
+            NAME ${VITIS_KERNEL_NAME}
+            FILE ${VITIS_KERNEL_FILE}
+            TYPE ${BUILT_TYPE}
+            CLOCK ${VITIS_KERNEL_CLOCK}
+            CONFIG ${VITIS_KERNEL_CONFIG}
+            INCLUDE ${VITIS_KERNEL_INCLUDE}
+            LINK
+          )
+        elseif(${VITIS_KERNEL_PACKAGE})
+          vitis_acceleration_kernel_aux(
+            NAME ${VITIS_KERNEL_NAME}
+            FILE ${VITIS_KERNEL_FILE}
+            TYPE ${BUILT_TYPE}
+            CLOCK ${VITIS_KERNEL_CLOCK}
+            CONFIG ${VITIS_KERNEL_CONFIG}
+            INCLUDE ${VITIS_KERNEL_INCLUDE}
+            PACKAGE
+          )
+        else()
+          vitis_acceleration_kernel_aux(
+            NAME ${VITIS_KERNEL_NAME}
+            FILE ${VITIS_KERNEL_FILE}
+            TYPE ${BUILT_TYPE}
+            CLOCK ${VITIS_KERNEL_CLOCK}
+            CONFIG ${VITIS_KERNEL_CONFIG}
+            INCLUDE ${VITIS_KERNEL_INCLUDE}
           )
         endif()
       endforeach()
@@ -108,24 +141,32 @@ endmacro()  # vitis_acceleration_kernel
 # file for the kernel e.g. src/zcu102.cfg.
 # :type target: string
 #
+# :param VITIS_KERNEL_AUX_CLOCK: Frequency in Hz at which the kernel should be
+# compiled by Vitis HLS.
+# See https://www.xilinx.com/html_docs/xilinx2020_2/vitis_doc/vitiscommandcompiler.html#mcj1568640526180__section_bh5_dg4_bjb
+# :type target: string
+#
 # :param VITIS_KERNEL_AUX_INCLUDE: one or muliple relative path directories whereto
 # search for header files. This option is passed to the OpenCL preprocessor.
 # Directories are relative to CMAKE_SOURCE_DIR.
 # :type target: string
 #
-# :param VITIS_KERNEL_AUX_PACKAGE: Invoke the -p (package) flag of v++ to generate
-# the corresponding artifacts for emulation.
+# :param VITIS_KERNEL_AUX_LINK: Invoke v++ linker in the
+# the corresponding artifacts.
 # :type target: bool
+#
+# :param VITIS_KERNEL_AUX_PACKAGE: Invoke the -p (package) flag of v++ to generate
+# the corresponding artifacts.
+# :type target: bool
+#
 #
 macro(vitis_acceleration_kernel_aux)
   # arguments
-  set(options PACKAGE)
-  set(oneValueArgs NAME FILE CONFIG TYPE)
+  set(options PACKAGE LINK)
+  set(oneValueArgs NAME FILE CONFIG TYPE CLOCK)
   set(multiValueArgs INCLUDE)
   cmake_parse_arguments(VITIS_KERNEL_AUX "${options}" "${oneValueArgs}"
                         "${multiValueArgs}" ${ARGN} )
-
-  message(STATUS "vitis_acceleration_kernel_swemu")
 
   # variables for the compilation process
   set(ENV{PLATFORM_REPO_PATHS} ${PLATFORM_REPO_PATHS})
@@ -134,7 +175,6 @@ macro(vitis_acceleration_kernel_aux)
   set(ENV{XILINX_HLS} ${XILINX_HLS})
   # set(ENV{PATH} "$ENV{PATH}:${XILINX_HLS}/bin")
 
-  # compile
   set(CMD "PLATFORM_REPO_PATHS=${PLATFORM_REPO_PATHS} ${VPP_PATH} -c -t ${VITIS_KERNEL_AUX_TYPE} --config " ${CMAKE_SOURCE_DIR} "/"
     ${VITIS_KERNEL_AUX_CONFIG} " -k " ${VITIS_KERNEL_AUX_NAME} " "
   )
@@ -157,149 +197,174 @@ macro(vitis_acceleration_kernel_aux)
     message(STATUS "No kernels built")
   else()
     # CMake configure time
+
     # Build
-    execute_process(
-      COMMAND
-        ${VPP_PATH} -c -t ${VITIS_KERNEL_AUX_TYPE}
-          --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
-          -k ${VITIS_KERNEL_AUX_NAME}
-          ${INCLUDE_DIRS}
-          "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_FILE}"
-          -o "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xo"
-      RESULT_VARIABLE
-          CMD_ERROR
-    )
+    ## pass clock flag if available
+    if ("${VITIS_KERNEL_AUX_CLOCK}" STREQUAL "")
+      execute_process(
+        COMMAND
+          ${VPP_PATH} -c -t ${VITIS_KERNEL_AUX_TYPE}
+            --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
+            -k ${VITIS_KERNEL_AUX_NAME}
+            ${INCLUDE_DIRS}
+            "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_FILE}"
+            -o "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xo"
+        RESULT_VARIABLE
+            CMD_ERROR
+      )
+    else()
+      execute_process(
+        COMMAND
+          ${VPP_PATH} -c --hls.clock ${VITIS_KERNEL_AUX_CLOCK} -t ${VITIS_KERNEL_AUX_TYPE}
+            --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
+            -k ${VITIS_KERNEL_AUX_NAME}
+            ${INCLUDE_DIRS}
+            "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_FILE}"
+            -o "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xo"
+        RESULT_VARIABLE
+            CMD_ERROR
+      )
+      # message("${VITIS_KERNEL_AUX_CLOCK}")  # debug
+    endif()
     message(STATUS "CMD_ERROR: " ${CMD_ERROR})
 
     # adjust final binary name if not "hw" (only "hw" build target is without suffix)
     set(BINARY_NAME "${VITIS_KERNEL_AUX_NAME}.xclbin")
     if (NOT ${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
       set(BINARY_NAME "${VITIS_KERNEL_AUX_NAME}.xclbin.${VITIS_KERNEL_AUX_TYPE}")
-      # get a temporary symlink pointing the binary name to allow/permit the
-      # v++ compilation process
-      # NOTE: v++ is sensitive to the artifact names.
-      # See ERROR: [v++ 60-2262] Unsupported input file type specified for
-      # --package option.
-      execute_process(
-        COMMAND
-          ln -s ${BINARY_NAME} ${VITIS_KERNEL_AUX_NAME}.xclbin
-      )
     endif()
 
     # Link
-    execute_process(
-      COMMAND
-        ${VPP_PATH} -l -t ${VITIS_KERNEL_AUX_TYPE}
-          --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
-          "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xo"
-          -o "${CMAKE_BINARY_DIR}/${BINARY_NAME}"
-    )
-
-    # package
-    if (${VITIS_KERNEL_AUX_PACKAGE})
+    if (${VITIS_KERNEL_AUX_LINK})
+      # message("linking")  # debug
       execute_process(
         COMMAND
-          ${VPP_PATH} -p -t ${VITIS_KERNEL_AUX_TYPE}
+          ${VPP_PATH} -l -t ${VITIS_KERNEL_AUX_TYPE}
             --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
-            "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xclbin"
-            --package.out_dir package
-            --package.no_image
-      )
-    endif()  # package
-
-    #
-    # dfx-mgr artifacts
-    #
-    # NOTE: dfx-mgr implementation in 2020.2.2 requires
-    # besides the xilinx binary kernel (.xclbin), to 
-    # provide also two additional files:
-    #   - .bit.bin raw bitstream
-    #   - .dtbo device tree blob overlay
-    #    
-    # extract the raw bitstream
-    run("xclbinutil --dump-section BITSTREAM:RAW:${VITIS_KERNEL_AUX_NAME}.bit.bin \
-        --input ${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xclbin --force")
-    # create 
-    run("cp ${FIRMWARE_DATA}/../device_tree/kernel_default.dtbo \
-            ${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.dtbo")
-    
-    # install
-    if (EXISTS ${CMAKE_BINARY_DIR}/${BINARY_NAME})
-      # install acceleration kernel, and ROS 2 related artifacts
-      install(
-        FILES
-          "${CMAKE_BINARY_DIR}/${BINARY_NAME}"
-          "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.dtbo"
-          "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.bit.bin"
-        DESTINATION
-          lib/${PROJECT_NAME}
+            "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xo"
+            -o "${CMAKE_BINARY_DIR}/${BINARY_NAME}"
       )
 
-      # package installation
+      if (NOT ${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
+        # get a temporary symlink pointing the binary name to allow/permit the
+        # v++ packaging process
+        # NOTE: v++ is sensitive to the artifact names.
+        # See ERROR: [v++ 60-2262] Unsupported input file type specified for
+        # --package option.
+        message("ln -s ${BINARY_NAME} ${VITIS_KERNEL_AUX_NAME}.xclbin")
+        execute_process(
+          COMMAND
+            ln -s ${BINARY_NAME} ${VITIS_KERNEL_AUX_NAME}.xclbin
+        )
+      endif()
+
+      # package
+      #   only makes sense if linked already
       if (${VITIS_KERNEL_AUX_PACKAGE})
+        # message("packaging")  # debug
+        execute_process(
+          COMMAND
+            ${VPP_PATH} -p -t ${VITIS_KERNEL_AUX_TYPE}
+              --config "${CMAKE_SOURCE_DIR}/${VITIS_KERNEL_AUX_CONFIG}"
+              "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xclbin"
+              --package.out_dir package
+              --package.no_image
+        )
+      endif()  # package
+
+      #
+      # dfx-mgr artifacts
+      #
+      # NOTE: dfx-mgr implementation in 2020.2.2 requires
+      # besides the xilinx binary kernel (.xclbin), to
+      # provide also two additional files:
+      #   - .bit.bin raw bitstream
+      #   - .dtbo device tree blob overlay
+      #
+      # extract the raw bitstream
+      run("xclbinutil --dump-section BITSTREAM:RAW:${VITIS_KERNEL_AUX_NAME}.bit.bin \
+          --input ${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.xclbin --force")
+      # create
+      run("cp ${FIRMWARE_DATA}/../device_tree/kernel_default.dtbo \
+              ${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.dtbo")
+
+      # install
+      if (EXISTS ${CMAKE_BINARY_DIR}/${BINARY_NAME})
+        # install acceleration kernel, and ROS 2 related artifacts
         install(
-          DIRECTORY
-            "${CMAKE_BINARY_DIR}/package"
+          FILES
+            "${CMAKE_BINARY_DIR}/${BINARY_NAME}"
+            "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.dtbo"
+            "${CMAKE_BINARY_DIR}/${VITIS_KERNEL_AUX_NAME}.bit.bin"
           DESTINATION
             lib/${PROJECT_NAME}
         )
 
-        # if hw_emu, symlink to "sim" folder
-        if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw_emu")
-          # # create emulation dir if it doesn't exist, then symlink which
-          # # this leads to various simlinks (as many as kernels) in a
-          # # compounded way. Not ideal. See below for an alternative that checks
+        # package installation
+        if (${VITIS_KERNEL_AUX_PACKAGE})
+          install(
+            DIRECTORY
+              "${CMAKE_BINARY_DIR}/package"
+            DESTINATION
+              lib/${PROJECT_NAME}
+          )
+
+          # if hw_emu, symlink to "sim" folder
+          if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw_emu")
+            # # create emulation dir if it doesn't exist, then symlink which
+            # # this leads to various simlinks (as many as kernels) in a
+            # # compounded way. Not ideal. See below for an alternative that checks
+            #
+            # execute_process(
+            #   COMMAND
+            #     mkdir ${FIRMWARE_DATA}/../emulation
+            # )
+            # execute_process(
+            #   COMMAND
+            #     ln -s ${CMAKE_BINARY_DIR}/package/sim ${FIRMWARE_DATA}/../emulation/sim
+            # )
+
+            set(EMULATIONSIMDIR "test -e ${FIRMWARE_DATA}/../emulation/sim || ")
+            run("${EMULATIONSIMDIR} ln -s ${CMAKE_BINARY_DIR}/package/sim ${FIRMWARE_DATA}/../emulation/sim")
+          endif()  # hw_emu
+
+          # if hw, symlink to BOOT.BIN
+          # NOTE: removes and creates a new symlink for each kernel, only the last
+          #       symlinked kernel will remain in firmware/ dir.
           #
-          # execute_process(
-          #   COMMAND
-          #     mkdir ${FIRMWARE_DATA}/../emulation
-          # )
-          # execute_process(
-          #   COMMAND
-          #     ln -s ${CMAKE_BINARY_DIR}/package/sim ${FIRMWARE_DATA}/../emulation/sim
-          # )
+          if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
+            # if symlink exists, delete
+            run("[ -L ${FIRMWARE_DATA}/../BOOT.BIN ] && unlink ${FIRMWARE_DATA}/../BOOT.BIN")
+            # set(EMULATIONSIMDIR "test -e ${FIRMWARE_DATA}/../BOOT.BIN || ")
+            # set(EMULATIONSIMDIR "[ -L ${FIRMWARE_DATA}/../BOOT.BIN ] && [ -e ${FIRMWARE_DATA}/../BOOT.BIN ]  && ")
 
-          set(EMULATIONSIMDIR "test -e ${FIRMWARE_DATA}/../emulation/sim || ")
-          run("${EMULATIONSIMDIR} ln -s ${CMAKE_BINARY_DIR}/package/sim ${FIRMWARE_DATA}/../emulation/sim")
-        endif()  # hw_emu
+            # create the new symlink
+            run("ln -s ${CMAKE_BINARY_DIR}/package/BOOT.BIN ${FIRMWARE_DATA}/../BOOT.BIN")
+          endif()  # hw_emu
+        endif()  # package installation
 
-        # if hw, symlink to BOOT.BIN
-        # NOTE: removes and creates a new symlink for each kernel, only the last
-        #       symlinked kernel will remain in firmware/ dir.
-        #
-        if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
-          # if symlink exists, delete
-          run("[ -L ${FIRMWARE_DATA}/../BOOT.BIN ] && unlink ${FIRMWARE_DATA}/../BOOT.BIN")
-          # set(EMULATIONSIMDIR "test -e ${FIRMWARE_DATA}/../BOOT.BIN || ")          
-          # set(EMULATIONSIMDIR "[ -L ${FIRMWARE_DATA}/../BOOT.BIN ] && [ -e ${FIRMWARE_DATA}/../BOOT.BIN ]  && ")
+        # if sw_emu, deploy "data" from firmware
+        if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "sw_emu")
+          install(
+            DIRECTORY
+              "${FIRMWARE_DATA}"
+            DESTINATION
+              lib/${PROJECT_NAME}
+          )
+        endif()  # sw_emu
+      endif()  # install
 
-          # create the new symlink
-          run("ln -s ${CMAKE_BINARY_DIR}/package/BOOT.BIN ${FIRMWARE_DATA}/../BOOT.BIN")
-        endif()  # hw_emu
-      endif()  # package installation
-
-      # if sw_emu, deploy "data" from firmware
-      if (${VITIS_KERNEL_AUX_TYPE} STREQUAL "sw_emu")
-        install(
-          DIRECTORY
-            "${FIRMWARE_DATA}"
-          DESTINATION
-            lib/${PROJECT_NAME}
-        )
-      endif()  # sw_emu
-    endif()  # install
-
-    # cleanup symlink for packaging
-    if (NOT ${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
-      # # Check before unlinking, see below
-      # execute_process(
-      #   COMMAND
-      #     unlink ${VITIS_KERNEL_AUX_NAME}.xclbin
-      # )
-      set(KERNELTOUNLINK "test -e ${VITIS_KERNEL_AUX_NAME}.xclbin && ")
-      run("${KERNELTOUNLINK} unlink ${VITIS_KERNEL_AUX_NAME}.xclbin")
-
-    endif()
+      # cleanup symlink for packaging
+      if (NOT ${VITIS_KERNEL_AUX_TYPE} STREQUAL "hw")
+        # # Check before unlinking, see below
+        # execute_process(
+        #   COMMAND
+        #     unlink ${VITIS_KERNEL_AUX_NAME}.xclbin
+        # )
+        set(KERNELTOUNLINK "test -e ${VITIS_KERNEL_AUX_NAME}.xclbin && ")
+        run("${KERNELTOUNLINK} unlink ${VITIS_KERNEL_AUX_NAME}.xclbin")
+      endif()
+    endif()  # link
   endif()  # NOKERNELS
 endmacro()  # vitis_acceleration_kernel_swemu
 
